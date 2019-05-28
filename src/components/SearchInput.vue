32b77cff2cbf1c2f5594ffa2f8bca6a60a63150a
Szymon Borucki <szymonborucki@op.pl>
2019-05-28 18:02:57 +0200

    styling for input compelted + get rid of console errors by passing props

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="searchWrapper">
-    <input id="search" name="search" v-model="searchValue" @input="handleInput">
-  </div>
+  <input id="search" name="search" :value="value" @input="handleChange" />
 </template>
 <script>
 export default {
-  name: "SearchInput"
+  name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.searchWrapper {
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  width: 250px;
 
   input {
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 300;
     height: 30px;
     border: 0;
     background: none;
-    border-bottom: 1px solid black;
-  }
-}
-</style>
+    border-bottom: 1px solid white;
+    transition: box-shadow 0.3s ease-out;
 
+    @media (mind-width: 1024px) {
+      font-weight: 400;
+    }
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 10px 20px -8px rbga(255, 255, 255, 0.5);
+  }
+
+</style>
