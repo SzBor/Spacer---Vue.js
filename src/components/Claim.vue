28d7e03c759c7f284bf28cc7afd1e3222350fc81
Szymon Borucki <szymonborucki@op.pl>
2019-05-28 16:33:21 +0200

    added claim with background

---
+++ src/components/Claim.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="claimWrapper">
+    <h1 class="claim">SPACER</h1>
+    <p class="subclaim">
+      Begin an amazing journey throughout the galaxy
+      and discover places you have never seen.
+    </p>
+    <p class="subclaim">Type in anything space related to launch!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Claim"
+};
+</script>
+<style lang="scss" scoped>
+.claimWrapper {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.claim {
+  font-size: 50px;
+  margin: 0;
+}
+
+.subclaim {
+  margin: 20px 0 0 0;
+  font-size: 13px;
+}
+</style>
+
--- src/router.js
+++ src/router.js
@@ -1,22 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Search from './views/Search.vue';
-import About from './views/About.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Search from "./views/Search.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Search',
+      path: "/",
+      name: "Search",
       component: Search,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-    
     },
   ],
 });
